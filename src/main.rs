d29cc6997e43be9c05b8f6949b17c5b5e06ec9be
Navaneeth Sharma <63489382+Navaneeth-Sharma@users.noreply.github.com>
2023-10-18 23:46:42 +0530

    reading via command line

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,18 @@ extern crate chrono;
 mod time_related;
 pub use time_related::time_related_fn;
 
+use std::env;
+use std::fs;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let query: &String = &args[1];
+    let filename: &String = &args[2];
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong!");
+
+    println!("Contents: {:?}", contents);
+
    time_related_fn(); 
 }
