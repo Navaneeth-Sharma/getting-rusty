f9ccc5edafeef0e3da31ac3141efae012566ed5d
Navaneeth Sharma <63489382+Navaneeth-Sharma@users.noreply.github.com>
2023-10-19 22:54:32 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,35 @@ pub use time_related::time_related_fn;
 
 use std::env;
 use std::fs;
+use std::process;
+
+struct Config {
+    query: String,
+    file: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str>{
+
+        if args.len() < 3 {
+            return Err("not enough arguments"); 
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let query: &String = &args[1];
-    let filename: &String = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err|{process::exit(1)
+    })
+    
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong!");
+    let contents = fs::read_to_string(config.filename).expect("Something went wrong!");
 
     println!("Contents: {:?}", contents);
 
